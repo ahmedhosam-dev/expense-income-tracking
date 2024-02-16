e614dfecca11d766b232c5f05e151a82c920980c
ahmedhosam-dev <ahmed.hossam.t.11@gmail.com>
2024-02-16 21:56:29 +0200

    Add register form and valid username error.

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.models import User
+import json
+from django.http import JsonResponse
 
 # Create your views here.
 
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'User name should only contain alphanumeric characters.'}, status=400)
+        
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'User name is alreay taken!'}, status=409)
+        
+        return JsonResponse({'username_valid': True})
+    
 class RegistrationView(View):
     def get(self, request):
         return render(request, 'authentication/register.html')
+    
